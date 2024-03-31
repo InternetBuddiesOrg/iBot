6b743598a922f5527e3fa6e8a841ceea4db4aec0
Zaappy <protozappy@zynet.org>
2024-03-31 01:57:50 -0600

    status update

--- events/ready.js
+++ events/ready.js
@@ -8,6 +8,6 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`[INFO] Logged in as ${client.user.tag}`);
-    client.user.setActivity('the french revolution', { type: ActivityType.Streaming, url: 'https://www.twitch.tv/protozappy' });
+    client.user.setActivity('the Word of the Day', { type: ActivityType.Watching });
   },
 };
