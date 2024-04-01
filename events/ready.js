91fad2240fee8f25d93cd63e0fac09d774561ddc
Zaappy <protozappy@zynet.org>
2024-03-31 20:44:38 -0600

    add online message in ready event

--- events/ready.js
+++ events/ready.js
@@ -10,5 +10,7 @@ module.exports = {
   execute(client) {
     console.log(`[INFO] Logged in as ${client.user.tag}`);
     client.user.setActivity('the Word of the Day', { type: ActivityType.Watching });
+    const devChannel = client.channels.cache.get('1099564476698726401');
+    devChannel.send('✅ iBot is now online');
   },
 };
