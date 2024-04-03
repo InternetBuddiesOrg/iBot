49f8f2ba56852c8f3302d5b3e4264a090bd85eab
Zaappy <protozappy@zynet.org>
2024-04-03 15:09:30 -0600

    fix ping.js/give up on fixing API ping because no one cares

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -8,7 +8,7 @@ module.exports = {
     .setName('ping')
     .setDescription('Shows your client ping'),
 
-  async execute(interaction, client) {
+  async execute(interaction) {
     const message = await interaction.deferReply({ fetchReply: true });
     const embed = new EmbedBuilder()
       .setColor('#F0CD40')
@@ -18,11 +18,6 @@ module.exports = {
         iconURL: interaction.user.displayAvatarURL(),
       })
       .addFields([
-        {
-          name: 'API Latency',
-          value: `${client.ws.ping} ms`,
-          inline: true,
-        },
         {
           name: 'Client Ping',
           value: `${message.createdTimestamp - interaction.createdTimestamp} ms`,
