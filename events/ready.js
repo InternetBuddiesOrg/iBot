c222419274b4fb7c757f05b441ff678a6aedcb56
Zaappy <protozappy@zynet.org>
2024-04-17 13:23:33 -0600

    edit bot status

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports = {
 
   execute(client) {
     console.log(`[INFO] Logged in as ${client.user.tag}`);
-    client.user.setActivity('the Word of the Day', { type: ActivityType.Watching });
+    client.user.setActivity('the word of the day', { type: ActivityType.Watching });
     const devChannel = client.channels.cache.get('1099564476698726401');
     const embed = new EmbedBuilder()
       .setColor('#68AB3F')
