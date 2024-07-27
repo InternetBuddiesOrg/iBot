b126fccfadd2d5a3ee116629ba59e43128e4e529
Zaappy <protozappy@zynet.org>
2024-07-27 17:41:06 -0600

    add status to login message

--- src/events/ready.js
+++ src/events/ready.js
@@ -14,25 +14,22 @@ module.exports = {
   execute(client) {
     console.log(`[INFO] Logged in as ${client.user.tag}`);
 
-    const devChannel = client.channels.cache.get('1099564476698726401');
-    const embed = new EmbedBuilder()
-      .setColor('#68AB3F')
-      .setTitle('Logged in')
-      .setTimestamp();
-    devChannel.send({ embeds: [embed] });
-
     const statusData = JSON.parse(fs.readFileSync(path.join(__dirname, '../commands/utility/statusLatest.json'), 'utf8'));
+    let icon;
     let message;
 
     switch (statusData.status) {
     case 'online':
       client.user.setStatus(PresenceUpdateStatus.Online);
+      icon = '<:online:1266485857653620836>';
       break;
     case 'idle':
       client.user.setStatus(PresenceUpdateStatus.Idle);
+      icon = '<:idle:1266485882261733506>';
       break;
     case 'dnd':
       client.user.setStatus(PresenceUpdateStatus.DoNotDisturb);
+      icon = '<:dnd:1266485896866172958>';
       break;
     }
 
@@ -58,6 +55,7 @@ module.exports = {
         type: ActivityType.Streaming,
         url: 'https://www.twitch.tv/protozappy',
       });
+      icon = '<:streaming:1266485909688287303>';
       message = 'Streaming ';
       break;
     case 'watching':
@@ -66,5 +64,14 @@ module.exports = {
       break;
     }
     console.log(`[EVNT] Set the status to: (${statusData.status}) ${message}${statusData.value}`);
+
+    const devChannel = client.channels.cache.get('1099564476698726401');
+    const embed = new EmbedBuilder()
+      .setColor('#68AB3F')
+      .setTitle('Logged in')
+      .setTimestamp()
+      .setDescription(`-# **Status:** ${icon} ${message}${statusData.value}`);
+
+    devChannel.send({ embeds: [embed], flags: [4096] }); // @silent message
   },
 };
